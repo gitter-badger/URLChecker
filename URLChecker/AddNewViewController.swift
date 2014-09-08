00d411b2496bbe1c9f84c1507dcd78aa8bb535d8
Grzegorz Synowiec <mikoj@mynwt.eu>
2014-09-08 20:13:55 +0200

    layout, SegmentedControlValueChanged

--- URLChecker/AddNewViewController.swift
+++ URLChecker/AddNewViewController.swift
@@ -9,36 +9,55 @@
 import UIKit
 
 class AddNewViewController: UIViewController {
-
+    
     @IBOutlet weak var regexpTextView: UITextView!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var regexpLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         setupControl()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     func setupControl() {
         regexpTextView.layer.borderWidth = 0.2
         regexpTextView.layer.cornerRadius = 8
         regexpTextView.layer.borderColor = UIColor.grayColor().CGColor
     }
     
+    @IBAction func typeSegmentedControlValueChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            urlTextField.placeholder = "Enter URL"
+            regexpLabel.hidden = false
+            regexpTextView.hidden = false
+        case 1:
+            urlTextField.placeholder = "Enter URL from download data"
+            regexpLabel.hidden = true
+            regexpTextView.hidden = true
+        default:
+            urlTextField.placeholder = "Enter URL"
+            regexpLabel.hidden = false
+            regexpTextView.hidden = false
+        }
+    }
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
